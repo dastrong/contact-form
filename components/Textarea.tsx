fd09c2189af56958194e84bf7e8f50d1493a6671
Daniel Strong <danielstrong.ds@gmail.com>
2021-07-26 23:36:58 +0900

    removed defaultValue

--- components/Textarea.tsx
+++ components/Textarea.tsx
@@ -23,7 +23,6 @@ export default function Textarea(props: Props) {
           id={props.id}
           rows={props.rows}
           className="shadow-sm focus:ring-indigo-500 focus:border-indigo-500 mt-1 block w-full sm:text-sm border border-gray-300 rounded-md"
-          defaultValue={''}
           value={props.value}
           onChange={props.onChange}
         />
